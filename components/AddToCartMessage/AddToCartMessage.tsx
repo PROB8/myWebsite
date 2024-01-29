81719770170e333aad7b772409299d59a390a6f8
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-01-28 23:57:41 -0500

    feat: add links to continue shopping or checkout

--- components/AddToCartMessage/AddToCartMessage.tsx
+++ components/AddToCartMessage/AddToCartMessage.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
+import Button from "../Button/Button";
+
 type AddToCartMessageProps = {
   setModalOpen: () => void;
 };
 export default function AddToCartMessage(props: AddToCartMessageProps) {
+    const {setModalOpen} = props;
   return (
     <div>
-      <h2>You&#39;ve added this to your cart!</h2>
+      <h2>You&#39;ve added an item to your cart!</h2>
+      <Link href="javascript:void(0);" onClick={setModalOpen}>
+        Continue Shopping
+      </Link>
+      <Link href="/checkout">
+        Checkout
+      </Link>
     </div>
   );
 }

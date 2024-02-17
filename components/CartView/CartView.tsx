fef811346f30016f66865c4c8583f289d98e27a9
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-02-17 16:21:56 -0500

    fix: updates to css and added loading dots

--- components/CartView/CartView.tsx
+++ components/CartView/CartView.tsx
@@ -1,18 +1,20 @@
 'use client';
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import useCart from 'hooks/useCart';
 import PageHeader from 'components/PageHeader/PageHeader';
-import sharedStyles from 'components/SharedCss/Images.module.scss';
+import sharedStyles from 'components/SharedCss/SharedCss.module.scss';
 import styles from './CartView.module.scss';
 import SmallItemPreview from 'components/SmallItemPreview/SmallItemPreview';
 import CounterInput from 'components/CounterInput/CounterInput';
 import CartItem from '@/types/cartItem';
 import loadPaypal from '@/utils/paypal';
 import PaypalCartItem from '@/types/paypalCartItem';
+import LoadingDots from '../LoadingDots/LoadingDots';
 
 export default function CartVeiw(): JSX.Element {
   const [addItem, cart, removeItem] = useCart();
-  useEffect(() => {
+  const [showLoadingDots, setShowLoadingDots] = useState(true)
+useEffect(() => {
     if (cart.length) {
       let paypalCart: PaypalCartItem[] = [];
       for (const item of cart) {
@@ -31,36 +33,44 @@ export default function CartVeiw(): JSX.Element {
         onSuccess: () => {
           console.log('i Succeeded');
         },
+      }).then(() => {
+        setShowLoadingDots(false)
       });
     }
   }, [cart]);
 
-  const { buttonsWrapper, cartWrapper } = styles;
+  const { buttonsWrapper, cartWrapper,objectEnterActive , objectEnter } = styles;
+  const { alwaysCentered } = sharedStyles;
   return (
-    <div id="cart" className={cart.length === 1 ? cartWrapper : ''}>
+    <div id="cart" className={cart.length === 1 || cart.length === 0 ? cartWrapper : ''}>
       <PageHeader headerName="Cart" hideLinks={true} />
-      {cart.map((item: CartItem) => {
-        const { title, price, description, imageUrl, id } = item;
-        const { cartItem } = styles;
-        return (
-          <div className={cartItem} key={id}>
-            <SmallItemPreview
-              key={id}
-              title={title}
-              price={price}
-              description={description}
-              imageUrl={imageUrl}
-              styles={styles}
-            />
-            <CounterInput
-              item={item}
-              addItem={addItem}
-              removeItem={removeItem}
-            />
-          </div>
-        );
-      })}
-      <div id="paypal-button-container" className={buttonsWrapper}></div>
+      <div className={showLoadingDots ? objectEnter : objectEnterActive}>
+        {cart.map((item: CartItem) => {
+          const { title, price, description, imageUrl, id } = item;
+          const { cartItem } = styles;
+          return (
+            <div className={cartItem} key={id}>
+              <SmallItemPreview
+                key={id}
+                title={title}
+                price={price}
+                description={description}
+                imageUrl={imageUrl}
+                styles={styles}
+              />
+              <CounterInput
+                item={item}
+                addItem={addItem}
+                removeItem={removeItem}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className={alwaysCentered}>
+        {showLoadingDots && <LoadingDots />}
+      </div>
+      <div id="paypal-button-container"className={showLoadingDots ? `${buttonsWrapper} ${objectEnter}` : `${buttonsWrapper} ${objectEnterActive}`}></div>
     </div>
   );
 }

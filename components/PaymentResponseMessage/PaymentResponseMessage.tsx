338429d85d786f017154a474c223777d22c108b8
Jaha  Naeem Gitonga <jahagitonga@Jahas-MacBook-Pro.local>
2024-02-22 20:08:01 -0500

    fix: added failure message

--- components/PaymentResponseMessage/PaymentResponseMessage.tsx
+++ components/PaymentResponseMessage/PaymentResponseMessage.tsx
@@ -1,3 +1,5 @@
+import styles from './PaymentResponseMessage.module.scss';
+
 type PaymentResponseProps = {
   paymentSuccessful: boolean;
 };
@@ -5,8 +7,26 @@ export default function PaymentResponse(
   props: PaymentResponseProps
 ): JSX.Element {
   const { paymentSuccessful } = props;
+  const { firstP, lastP } = styles;
   if (!paymentSuccessful) {
-    return <div>Was unsuccessful</div>;
+    return (
+      <div>
+        <p className={firstP}>
+          Unfortunately, we were unable to process your order. Please make
+          another attempt to purchase.
+        </p>
+
+        <p>
+          You have <strong>not</strong> been charged for this attempt.
+        </p>
+
+        <p className={lastP}>
+          If you continue to exprience issues purchasing, please
+          contact support{' '}
+          <a href="mailto:gtngbooks@gmail.com">gtngbooks@gmail.com</a>.
+        </p>
+      </div>
+    );
   }
   return <div>in here Payment Response Was successful</div>;
 }
